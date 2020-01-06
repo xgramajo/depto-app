7a2d925fee27a7c9e3c37dec66ec7109eec51867
Xavier Gramajo <Xavier.Gramajo@datco.net>
2020-01-06 17:46:40 -0300

    Visuales

--- react-client/src/components/Main.js
+++ react-client/src/components/Main.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import NewsForm from './NewsForm';
 import NewsCard from './NewsCard';
-
-//<Route exact path='/' component={Home}></Route>
+import Login from './Login';
 
 const Main = () => (
     <Switch>
-      
+      <Route exact path='/login' component={Login}></Route>
       <Route exact path='/news' component={NewsCard}></Route>
       <Route exact path='/news/create-news' component={NewsForm}></Route>
     </Switch>
--- react-client/src/components/Navigation.js
+++ react-client/src/components/Navigation.js
@@ -1,17 +1,104 @@
-import React from "react";
+import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
-const Navigation = () => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-    return (
-        <nav>
-        <ul>
-          <li><NavLink to='/'>Inicio</NavLink></li>
-          <li><NavLink to='/news'>Noticias</NavLink></li>
-          <li><NavLink to='/news/create-news'>Crear Noticia</NavLink></li>
-        </ul>
-      </nav>
-    )
-};
+export default function Navigation() {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
-export default Navigation
+  const handleChange = event => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} onClick={handleMenu} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Photos
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>
+                  <NavLink to='/news/create-news'>Crear Noticia</NavLink>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <NavLink to='/news'>Noticias</NavLink>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <NavLink to='/login'>login</NavLink>
+                </MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
+          label={auth ? 'Logout' : 'Login'}
+        />
+      </FormGroup>
+    </div>
+  );
+}
